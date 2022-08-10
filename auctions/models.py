26bec8252809ce6120d07dd3f45874c31c7f00eb
Yesych Dmytro <dmytro.yesych@globallogic.com>
2022-08-11 00:15:08 +0300

    created models

---
+++ auctions/models.py
@@ -0,0 +1,98 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from datetime import datetime, timedelta
+from django.utils import timezone
+
+class User(AbstractUser):
+    def __str__(self):
+        return f"{self.username}"
+
+#this model reperents the Types of the auctions
+class Category(models.Model):
+    name = models.CharField(max_length=32)
+
+    def __str__(self):
+        return f"{self.name}"
+
+    @property
+    #getter modification to get the count of active auctions of the specifyed class
+    def count_active_auctions(self):
+        return Auction.objects.filter(category=self).count()
+
+#this model represents the auction itself
+class Auction(models.Model):
+    item_name        = models.CharField(max_length=64)
+    item_description = models.TextField(max_length=800)
+    image            = models.ImageField(
+                          blank = True,
+                          null = True,
+                          upload_to =''
+                       )
+
+    category         = models.ForeignKey(
+                          Category,
+                          blank=True,
+                          null=True,
+                          on_delete=models.SET_NULL,
+                          related_name="auctions"
+                       )
+
+    start_time       = models.DateTimeField()
+    end_time         = models.DateTimeField()
+
+    DURATIONS = (
+        (3, "Three Days"),
+        (7, "One Week"),
+        (14, "Two Weeks"),
+        (28, "Four Weeks")
+    )
+
+    duration         = models.IntegerField(choices=DURATIONS)
+    ended_manually   = models.BooleanField(default=False)
+    start_bid        = models.DecimalField(
+                          max_digits=10,
+                          decimal_places=2,
+                       )
+    user             = models.ForeignKey(
+                          User,
+                          on_delete=models.CASCADE,
+                          related_name="auctions"
+                       )
+    watchers         = models.ManyToManyField(
+                          User,
+                          blank=True,
+                          related_name="watchlist"
+                       )
+        
+    def __str__(self):
+        return f"Auction #{self.id}: {self.item_name} ({self.user.username})"
+
+    def save(self, *args, **kwargs):
+        self.start_time = datetime.now()
+        self.end_time = self.start_time + timedelta(days=self.duration)
+        super().save(*args, **kwargs) # call existing save() method
+
+    def is_finshed(self):
+        if self.ended_manually or self.end_time < timezone.now():
+            return True
+        else:
+            return False
+
+
+class Bid(models.Model):
+    amount  = models.DecimalField(max_digits=10, decimal_places=2)
+    user    = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bids")
+    auction = models.ForeignKey(Auction, on_delete=models.CASCADE, related_name="bids")
+
+    def __str__(self):
+        return f"Bid #{self.id}: {self.amount} on {self.auction.item_name} by {self.user.username}"
+
+
+class Comment(models.Model):
+    message = models.TextField(max_length=255)
+    time    = models.DateTimeField(auto_now_add=True)
+    user    = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    auction = models.ForeignKey(Auction, on_delete=models.CASCADE, related_name="comments")
+
+    def __str__(self):
+        return f"Comment #{self.id}: {self.user.username} on {self.auction.item_name}: {self.message}"
