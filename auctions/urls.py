c9c0e23dc3e999529daf268fe155976fb20b4bfb
Yesych Dmytro <dmytro.yesych@globallogic.com>
2022-08-12 01:01:27 +0300

    style and basic functions are implemented

--- auctions/urls.py
+++ auctions/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register")
+    path("register", views.register, name="register"),
+    path("category_list", views.category_list, name="category_list"),
+    path("<int:id>/category", views.category, name="category"),
+    path("<int:id>/auction", views.auction, name="auction"),
 ]
--- auctions/views.py
+++ auctions/views.py
@@ -1,14 +1,47 @@
+from datetime import datetime
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from django.forms import ModelForm, TextInput
+from django.utils import timezone
 
-from .models import User
 
+from .models import Auction, Bid, Category, Comment, User
+
+#listing all active auctions 
 
 def index(request):
-    return render(request, "auctions/index.html")
+    active_auctions = Auction.objects.filter(ended_manually=False, end_time__gte=datetime.now())
+
+    return render(request, "auctions/index.html",{
+        "auctions": active_auctions,
+        "title": "Curently active"
+    })
+
+class BidForm(ModelForm):
+    class Meta:
+        model = Bid
+        fields = ['amount']
+
+    def __init__(self, *args, **kwargs):
+        super(BidForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control m-2'
+
+class CommentForm(ModelForm):
+
+    class Meta:
+        model = Comment
+        fields = ['message']
+
+    def __init__(self, *args, **kwargs):
+        super(CommentForm, self).__init__(*args, **kwargs)
+        self.visible_fields()[0].field.widget.attrs['class'] = 'form-control w-75 h-75'
+
 
 
 def login_view(request):
@@ -61,3 +94,45 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+
+def category(request, id):
+    params = {}
+    params["categoryes"] = Category.objects.filter(~Q(pk=id))
+    params["category_chousen"] = category_chousen = Category.objects.get(pk=id)
+    params["auctions"] = Auction.objects.filter(ended_manually=False, end_time__gte=datetime.now(), category=category_chousen)
+
+    return render(request ,"auctions/category.html", params)
+
+def category_list(request):
+    params = {}
+    params["categories"] = Category.objects.all()
+
+    return render(request ,"auctions/category_list.html", params)
+
+def auction(request, id):
+    # check that auction exists
+    try:
+        auction = Auction.objects.get(id=id)
+    except:
+        return HttpResponse("Entry does not exist")
+
+
+    context = {}
+    context["auction"] = auction
+
+    if auction.is_finshed():
+        context["ended"] = True
+        return render(request, "auctions/auction.html", context)
+
+    context["ended"] = False
+
+    # calculate times
+    time_remaining = auction.end_time - timezone.now()
+    context["days"] = time_remaining.days
+    context["hours"] = int(time_remaining.seconds / 3600)
+    context["minutes"] = int(time_remaining.seconds / 60 - (context["hours"] * 60))
+    context["bid_form"] = BidForm()
+    context["comment_form"] = CommentForm()
+
+    return render(request, "auctions/auction.html", context)
